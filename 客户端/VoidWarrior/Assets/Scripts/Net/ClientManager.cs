f966b481c6c69731e163d9c1603b1853366213a3
zzxxzxzzxx <627012408@qq.com>
2019-03-15 22:39:33 +0800

    网络

---
+++ 客户端/VoidWarrior/Assets/Scripts/Net/ClientManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System;
+
+/// <summary>
+/// ClientManager
+/// 管理服务器端的Socket连接，继承自BaseManager（管理器基类）
+/// </summary>
+public class ClientManager : BaseManager
+{
+    #region 构造方法
+    /// <summary>
+    /// 构造方法
+    /// </summary>
+    /// <param name="facade">facade中介者</param>
+    public ClientManager(GameFacade facade) : base(facade) { }
+    #endregion
+
+    #region 成员变量
+    /// <summary>
+    /// 连接IP地址
+    /// </summary>
+    private const string IP = "127.0.0.1";
+
+    /// <summary>
+    /// 连接端口号
+    /// </summary>
+    private const int PORT = 6688;
+
+    /// <summary>
+    /// Socket连接
+    /// </summary>
+    private Socket clientSocket;
+
+    /// <summary>
+    /// 消息类
+    /// </summary>
+    private Message msg = new Message();
+    #endregion
+
+    #region 提供的方法
+    /// <summary>
+    /// 重写初始化方法
+    /// </summary>
+    public override void OnInit()
+    {
+        base.OnInit(); //调用父类的初始化方法
+
+        clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); //创建Socket连接，使用ipv4，Socket流方式，Tcp协议
+        try
+        {
+            clientSocket.Connect(IP, PORT); //尝试连接服务器
+            Start(); //开始异步接收消息
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning("无法连接到服务器端，请检查您的网络！！" + e); //异常提醒
+        }
+    }
+
+    /// <summary>
+    /// 发送请求
+    /// </summary>
+    /// <param name="data">请求数据</param>
+    public void SendRequest(string data)
+    {
+        byte[] bytes = Message.PackData(data); //装包
+        clientSocket.Send(bytes); //向服务器发送
+    }
+
+    /// <summary>
+    /// 重写销毁方法
+    /// </summary>
+    public override void OnDestroy()
+    {
+        base.OnDestroy(); //调用父类的销毁方法
+        try
+        {
+            clientSocket.Close(); //关闭Socket连接
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning("无法关闭跟服务器端的连接！！" + e); //异常提醒
+        }
+    }
+    #endregion
+
+    #region 私有方法
+    /// <summary>
+    /// 开始异步接收client信息
+    /// </summary>
+    private void Start()
+    {
+        clientSocket.BeginReceive(msg.Data, msg.StartIndex, msg.RemainSize, SocketFlags.None, ReceiveCallback, null);
+    }
+
+    /// <summary>
+    /// 接收信息回调方法
+    /// </summary>
+    /// <param name="ar">接收的信息</param>
+    private void ReceiveCallback(IAsyncResult ar)
+    {
+        try
+        {
+            if (clientSocket == null || clientSocket.Connected == false) return; //失去连接直接返回
+            int count = clientSocket.EndReceive(ar); //获取接收信息数量
+            msg.ReadMessage(count, OnProcessDataCallback); //消息解读
+
+            Start(); //继续异步监听
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e); //异常提醒
+        }
+    }
+
+    /// <summary>
+    /// 将解读出的消息返回给响应方法
+    /// </summary>
+    /// <param name="actionCode">客户端处理方式</param>
+    /// <param name="data">响应数据</param>
+    private void OnProcessDataCallback(ActionCode actionCode, string data)
+    {
+        facade.HandleReponse(actionCode, data);
+    }
+    #endregion
+}
---
+++ 客户端/VoidWarrior/Assets/Scripts/Net/Message.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Text;
+using System.Linq;
+using Newtonsoft.Json;
+
+/// <summary>
+/// Message
+/// 消息类
+/// 用来存放网络传输的字节
+/// </summary>
+public class Message
+{
+    #region 成员变量
+    /// <summary>
+    /// 存放传输字节数组
+    /// </summary>
+    private byte[] data = new byte[1024];
+
+    /// <summary>
+    /// 存取的字节数据
+    /// </summary>
+    private int startIndex = 0;
+
+    //public void AddCount(int count)
+    //{
+    //    startIndex += count;
+    //}
+
+    /// <summary>
+    /// 传输字节数组属性
+    /// </summary>
+    public byte[] Data
+    {
+        get { return data; }
+    }
+
+    /// <summary>
+    /// 存取字节数属性
+    /// </summary>
+    public int StartIndex
+    {
+        get { return startIndex; }
+    }
+
+    /// <summary>
+    /// 剩余字节数属性
+    /// </summary>
+    public int RemainSize
+    {
+        get { return data.Length - startIndex; }
+    }
+    #endregion
+
+    #region 提供的方法
+    /// <summary>
+    /// 解析数据或者叫做读取数据
+    /// 处理粘包和分包问题
+    /// </summary>
+    public void ReadMessage(int newDataAmount, Action<ActionCode, string> processDataCallback)
+    {
+        startIndex += newDataAmount; //总量加上新接收数量
+        while (true)
+        {
+            if (startIndex <= 4) return; //前4字节代表新信息个数，小于4字节直接返回
+            int count = BitConverter.ToInt32(data, 0); //从数据中0号位置二进制转换出四字节
+            if (startIndex >= count) //该条数据是否全部接收
+            {
+                //Console.WriteLine(startIndex);
+                //Console.WriteLine(count);
+                //string s = Encoding.UTF8.GetString(data, 4, count);
+                //Console.WriteLine("解析出来一条数据：" + s);
+                string s = Encoding.UTF8.GetString(data, 4, count - 4); //将剩余数据从二进制转换出来
+                ActionData json = JsonConvert.DeserializeObject<ActionData>(s);
+                processDataCallback((ActionCode)Enum.Parse(typeof(ActionCode), json.Action), json.JsonData); //回调响应解析出来的数据
+                Array.Copy(data, count, data, 0, startIndex - count); //剩余数据前移
+                startIndex -= count; //更新总量
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    //public static byte[] PackData(ActionCode actionCode, string data)
+    //{
+    //    byte[] requestCodeBytes = BitConverter.GetBytes((int)actionCode);
+    //    byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+    //    int dataAmount = requestCodeBytes.Length + dataBytes.Length;
+    //    byte[] dataAmountBytes = BitConverter.GetBytes(dataAmount);
+    //    byte[] newBytes = dataAmountBytes.Concat(requestCodeBytes).ToArray<byte>();//Concat(dataBytes);
+    //    return newBytes.Concat(dataBytes).ToArray<byte>();
+    //}
+
+    /// <summary>
+    /// 静态方法，装包
+    /// </summary>
+    /// <param name="data">传输数据</param>
+    /// <returns></returns>
+    public static byte[] PackData(string data)
+    {
+        //转换二进制
+        byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+
+        int dataAmount = dataBytes.Length + 4; //计算数据长度
+        //组合数据，即装包
+        byte[] dataAmountBytes = BitConverter.GetBytes(dataAmount);
+        //byte[] newBytes = dataAmountBytes.Concat(requestCodeBytes).ToArray<byte>();//Concat(dataBytes);
+        //return newBytes.Concat(dataBytes).ToArray<byte>();
+        return dataAmountBytes.ToArray<byte>()
+            .Concat(dataBytes).ToArray<byte>();
+    }
+    #endregion
+}
